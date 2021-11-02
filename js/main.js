1e0d02b404223a244cacc02379a2237e84d5c08f
mdfaysalmia <faysalmia.dev@gmail.com>
2021-11-02 16:14:30 +0600

    Initialize Football template

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function($){
+	"use strict"
+		// Gallery js
+		var mixer = mixitup('.gallery-content');
+		$('.popup').magnificPopup({
+			type:'image',
+			gallery:{
+				enabled:true,
+			  }
+		});
+
+
+})(jQuery);
